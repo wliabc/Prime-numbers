57390fed0d1b41eee4ef36789668ab22fedfe981
wliabc <43542590+wliabc@users.noreply.github.com>
2018-09-29 07:17:45 -0400

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,14 +1,14 @@
-myresult ='not a prime number'
-mynum= 18
-prime = True
+#ask user for an integer and tell the user whether it is a prime number or not.  
+mynum= int(input("Please enter an integer you want to check"))
+isprime = True
 i=2
 while i < mynum:
      if (mynum % i)==0:
-        prime = False
+        isprime = False
         break
      i=i+1
       
-if prime == True:
+if isprime == True:
      print(str(mynum) + " is a prime number")
 else:
      print(str(mynum) + " is not a prime number")
